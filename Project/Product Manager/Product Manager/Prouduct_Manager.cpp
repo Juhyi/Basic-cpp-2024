8f9e3d6d5191ba980dd4f14ab44c46d50627a3d2
Juhyi <dlwngml201@naver.com>
2024-04-29 17:08:42 +0900

    리드미 정리

--- Project/Product Manager/Product Manager/Prouduct_Manager.cpp
+++ Project/Product Manager/Product Manager/Prouduct_Manager.cpp
@@ -1,29 +1,9 @@
-// date : 2024-03-08
-// file : test07_Homework.cpp
-// desc :
-
 /*
-   * Parent class
-     class Product : id, price, producer
-   * Child class
-     class Book : ISBN, author, title         // 978-89-001-0001-1
-     class Handphone : model, RAM
-     class Computer : model, cpu, RAM
-   * 객체포인터 배열 사용: product[100]
-   * string 클래스 사용 가능
+    File:Product_Manager.c
+    Created: 2024-03-06
+    Author: 이주희
 */
 
-
-/*
-   * 메인화면
-   -----상품관리 프로그램------
-   1 상품추가 2 상품출력 3 상품검색 0 종료
-   >
-
-   1 또는 2 또는 3을 선택한 경우
-   1 책 2 핸드폰 3 컴퓨터
-   선택 목록을 띄워 해당 상품을 선택한 후에 추가, 출력 또는 검색이 실행된다
-*/
 
 #include <iostream>
 #include <string>
